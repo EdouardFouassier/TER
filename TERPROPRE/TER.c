3ff317e83497d9dff48ee45027ada0136a0b5d5b
Frostyz <remi.navarro@ens.uvsq.fr>
2019-05-02 00:25:44 +0200

    calcul

--- TERPROPRE/TER.c
+++ TERPROPRE/TER.c
@@ -31,11 +31,10 @@ double multi(int algo,int nb,int periode,int nbTask){
 
 int main(int argc,char** argv){
 	//affichePeriode(periodes[0]);
-	//	printf("\n");
-	float temps;
+//	printf("\n");
+	float temps1=0,temps2=0,moyenne1=0,moyenne2=0,acc,amelio,eff;
     clock_t t1, t2;
 
-    t1 = clock();
 	int periode=atoi(argv[1]),cycle=atoi(argv[3]),nb=atoi(argv[4]),nbTask=atoi(argv[2]);
 	
 	int maxTask=periode/cycle;
@@ -43,10 +42,17 @@ int main(int argc,char** argv){
 	double taux[2][maxTask];
 	int i=1;
 	
-	//#pragma omp parallel for private(i)
+	#pragma omp parallel for private(i)
 	for(i=1;i<=maxTask;i++){
+		t1 = clock();
 		taux[0][i-1]=multi(0,nb,periode,i);
+		moyenne1+=taux[0][i-1];
+		t2 = clock();
+		temps1 += (float)(t2-t1)/CLOCKS_PER_SEC;
 		taux[1][i-1]=multi(1,nb,periode,i);
+		moyenne2+=taux[1][i-1];
+		t1 = clock();
+		temps2 += (float)(t1-t2)/CLOCKS_PER_SEC;
 	}
 	
 	
@@ -55,9 +61,14 @@ int main(int argc,char** argv){
 		fprintf(statout,"%d	%f 	%f \n",i,taux[0][i-1],taux[1][i-1]);
 	}
 	fclose(statout);
-   t2 = clock();
-    temps = (float)(t2-t1)/CLOCKS_PER_SEC;
-    printf("temps = %f\n", temps);
+   	temps1=temps1/maxTask;
+   	temps2=temps2/maxTask;
+   	moyenne1=moyenne1/maxTask;
+   	moyenne2=moyenne2/maxTask;
+   	acc=(temps2/temps1);	
+   	amelio=(moyenne2/moyenne1);
+   	eff=amelio/acc;
+    printf("temps FirstFit= %f \n temps algoLourd= %f \n acceleration= %f \n reussite FirstFit= %f \n reussite algoLourd=%f \n amelioration= %f \n Efficacité= %f \n ", temps1,temps2,acc,moyenne1,moyenne2,amelio,eff);
 	/*
 	float temps;
     clock_t t1, t2;
