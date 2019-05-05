ecd77dc0f7fdf594caf88b0fedd7af50e4c63e41
EdouardFouassier <efouassier78@gmail.com>
2019-05-05 21:57:13 +0200

    On remet le canapé

---
+++ TERPLUS/FirstFit.h
@@ -0,0 +1,8 @@
+#ifndef FIRSTFIT
+#define FIRSTFIT
+#include "bibli.h"
+
+TaskTab FirstFit(TaskTab tasktab,int periodeMax,int nbTask);
+
+
+#endif
---
+++ TERPLUS/gen.h
@@ -0,0 +1,12 @@
+#ifndef GEN
+#define GEN
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <math.h>
+
+void gen(int periode,int cycle,int nbFile );
+
+
+#endif
